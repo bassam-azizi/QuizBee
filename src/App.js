bf3d17df5834a17efd0144c69553d44eff7bcf94
BassamAzizi <101010bassam@gmail.com>
2020-01-11 15:54:17 +0100

    building the interface of the quiz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./styles.css";
+import "./assets/style.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>QuizBee</h1>
+    <div className="container">
+      <div className="title">QuizBee</div>
     </div>
   );
 }
